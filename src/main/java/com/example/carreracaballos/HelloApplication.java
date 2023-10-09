0313a425cb0b0c4d99be2fbaf64d391536b4a1da
DAM <ivanmartingomez2511@gmail.com>
2023-10-09 14:11:00 +0200

    hola

---
+++ src/main/java/com/example/carreracaballos/HelloApplication.java
@@ -0,0 +1,37 @@
+package com.example.carreracaballos;
+
+import com.gluonhq.charm.glisten.control.ProgressBar;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class HelloController {
+
+    @FXML
+    private Button botonIniciar;
+
+    @FXML
+    private ProgressBar caballo1;
+
+    @FXML
+    private ProgressBar caballo2;
+
+    @FXML
+    private ProgressBar caballo3;
+
+    @FXML
+    private ProgressBar caballo4;
+
+    @FXML
+    private ProgressBar caballo5;
+
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    void pulsarBotonIniciar(ActionEvent event) {
+
+    }
+
+}
